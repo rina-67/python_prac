c7f7eec8909b1f85c794e0891a803812ceceafd7
rng73 <rnszk@outlook.com>
2020-07-01 21:39:20 +0900

    :tada:first commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = "Risu"
+    return render_template('hello.html', title='flask test', name=name)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
